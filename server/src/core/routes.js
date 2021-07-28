c2792153684810b16a2112d78a98585ba1cfd041
Nikita Babko <niki.babko@gmail.com>
2021-07-28 05:19:12 -0700

    Deleted server dir.

--- server/src/core/routes.js
+++
@@ -1,8 +0,0 @@
-const AuthController = require('../controllers/AuthController');
-const registerValidation = require('../middlewares/registerValidation');
-
-const createRoutes = (app) => {
-  app.post('/api/register', registerValidation, AuthController.register);
-};
-
-module.exports = createRoutes;
